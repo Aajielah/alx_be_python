8cd219c3e426d06b6422bce14be6372af87713d0
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2025-11-09 10:55:52 +0100

    Go on

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,14 +1,14 @@
 
-number1 = 10
-number2 = 5
+numbers1 = 10
+numbers2 = 5
 
 
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2
+sum_result = numbers1 + numbers2
+difference_result = numbers1 - numbers2
+product_result = numbers1 * numbers2
 
 
-print(f"Sum of {number1} and {number2} is {sum_result}")
-print(f"Difference of {number1} and {number2} is {difference_result}")
-print(f"Product of {number1} and {number2} is {product_result}")
+print(f"Sum of {numbers1} and {numbers2} is {sum_result}")
+print(f"Difference of {numbers1} and {numbers2} is {difference_result}")
+print(f"Product of {numbers1} and {numbers2} is {product_result}")
 
