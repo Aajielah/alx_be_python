52a8bb239fe787c5ea8af0235aebbc74e0c289d3
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2025-11-26 20:12:36 +0100

    repaired

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,14 @@
 def perform_operation(num1, num2, operation):
     match operation:
-        case "+":
-            result = float(num1) + num2
-            print(f"The result is {result}")
-        case "-":
-            result = num1 - num2
-            print(f"The result is {result}")
-        case "*":
-            result = num1 * num2
-            print(f"The result is {result}")
-        case "/":
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
             if num2 == 0:
-                print("Cannot divide by zero.")
-            else:
-                result = num1 / num2
-                print(f"The result is {result}")
-
+                return "Cannot divide by zero"
+            return num1 / num2
+        case _:
+            return "Invalid operation"
