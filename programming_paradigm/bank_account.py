c11aed64e5fb8de3162c3cb3b0c13d69447d2e41
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-01-01 13:20:05 +0100

    I hope I did it well

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.__account_balance = account_balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+    def withdraw(self, amount):
+        if amount < self.__account_balance:
+            return True
+        if amount > self.__account_balance:
+            return False
+        self.__account_balance -= amount
+
+
+    def display_balance(self):
+
+        print(f"Your account balance is:{self.__account_balance}")
+
+
