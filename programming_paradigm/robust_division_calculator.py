a8cb9bda70e951b1c09ad0d49f7ee2f32bf5ea60
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-01-01 14:56:53 +0100

    I need to swear

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,10 +4,10 @@ def safe_divide( numerator: float, denominator: float ):
         denominator = float(denominator)
         division = numerator / denominator
     except ZeroDivisionError:
-        print(f"Error: Cannot divide by zero")
+        print(f"Error: Cannot divide by zero.")
         return None
     except ValueError:
-        print(f"Error: Please enter numeric values only")
+        print(f"Error: Please enter numeric values only.")
     else:
         print(f"The result of the division is {division}")
         return division
