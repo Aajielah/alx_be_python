444ab0f4fd3bbb18f37ee0f8482e872a5675e385
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-01-01 13:22:22 +0100

    Minor mistakes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
 
     def display_balance(self):
 
-        print(f"Your account balance is:{self.__account_balance}")
+        print(f"Current Balance:{self.__account_balance}")
 
 
